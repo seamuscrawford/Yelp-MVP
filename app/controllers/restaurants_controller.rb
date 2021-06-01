5d217f221f6ba8a0cca5dddb429630e93bd91f02
seamus crawford <seamuscrawford@hotmail.co.uk>
2021-06-01 09:27:40 +0800

    Restaurant review embedded in Show page for each restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,6 +13,7 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
   end
 
   private
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,20 +1,22 @@
 class ReviewsController < ApplicationController
-
+    before_action :find_restaurant
 
     def new
-        @restaurant = Restaurant.find(params[:restaurant_id])
         @review = Review.new
     end
     
     def create
         @review = Review.new(review_params)
-        @restaurant = Restaurant.find(params[:restaurant_id])
         @review.restaurant = @restaurant
         @review.save
-        redirect_to restaurants_path(@restaurant)
+        redirect_to restaurant_path(@restaurant)
     end
 
     private
+
+    def find_restaurant
+        @restaurant = Restaurant.find(params[:restaurant_id])
+    end
 
     def review_params
         params.require(:review).permit(:content)
